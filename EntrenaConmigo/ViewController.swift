de44e5b4efc22a6e68099e1b92a08f8d81def073
LuisArceBenites <luismi10arce@gmail.com>
2026-04-04 09:26:33 -0400

    Agrego Pantalla Registro de Usuario

--- EntrenaConmigo/ViewController.swift
+++ EntrenaConmigo/ViewController.swift
@@ -9,14 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var email: UIView!
+    
+    @IBOutlet weak var email: UITextField!
     @IBOutlet weak var password: UITextField!
     @IBOutlet weak var iniciarSesion: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+  
 }
 
