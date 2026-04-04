9ea7d3198e183aa306a610cabe5b85a038d653ef
LuisArceBenites <luismi10arce@gmail.com>
2026-04-03 22:44:33 -0400

    Conecto con el controller

--- EntrenaConmigo/ViewController.swift
+++ EntrenaConmigo/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var email: UIView!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var iniciarSesion: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
